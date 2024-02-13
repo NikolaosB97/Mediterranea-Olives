69e725c0a493265aca5cb68905540d37e6c48b37
Bennylnz <bennylanza@gmail.com>
2024-02-13 17:52:25 +0100

    sistemata pagina prodotti

---
+++ resources/views/prodotti.blade.php
@@ -0,0 +1,110 @@
+<x-layout>
+<div class="container-fluid vh-100">
+    <h1 class="text-center container-product">I nostri prodotti</h1>
+    <div class="container-fluid d-flex justify-content-center ">
+        <div class="card me-5">
+            <div class="imgbx">
+                <img src="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/7e90a06d-13fa-46d0-8cde-e3f3f0c61363/air-max-pre-day-be-true-shoes-gjZtQJ.png" alt="">
+                
+            </div>
+            <div class="details">
+                <h3>Nome dell'oliva</h3>
+                <h4>Dettaglio prodotto</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit
+                    . Maxime distinctio omnis necessitatibus
+                </p>
+                <h4>Formati disponibili</h4>
+                <ul>
+                    <li>36</li>
+                    <li>38</li>
+                    <li>40</li>
+                    <li>42</li>
+                    <li>44</li>
+                </ul>
+                <div class="group">
+                    {{-- <h2><sup>€</sup>199<small>.99</small></h2> --}}
+                    <a href="#">Leggi tutto</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="card me-5">
+            <div class="imgbx">
+                <img src="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/7e90a06d-13fa-46d0-8cde-e3f3f0c61363/air-max-pre-day-be-true-shoes-gjZtQJ.png" alt="">
+                
+            </div>
+            <div class="details">
+                <h3>Nome dell'oliva</h3>
+                <h4>Dettaglio prodotto</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit
+                    . Maxime distinctio omnis necessitatibus
+                </p>
+                <h4>Formati disponibili</h4>
+                <ul>
+                    <li>36</li>
+                    <li>38</li>
+                    <li>40</li>
+                    <li>42</li>
+                    <li>44</li>
+                </ul>
+                <div class="group">
+                    {{-- <h2><sup>€</sup>199<small>.99</small></h2> --}}
+                    <a href="#">Leggi tutto</a>
+                </div>
+            </div>
+        </div> 
+        
+        <div class="card me-5">
+            <div class="imgbx">
+                <img src="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/7e90a06d-13fa-46d0-8cde-e3f3f0c61363/air-max-pre-day-be-true-shoes-gjZtQJ.png" alt="">
+                
+            </div>
+            <div class="details">
+                <h3>Nome dell'oliva</h3>
+                <h4>Dettaglio prodotto</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit
+                    . Maxime distinctio omnis necessitatibus
+                </p>
+                <h4>Formati disponibili</h4>
+                <ul>
+                    <li>36</li>
+                    <li>38</li>
+                    <li>40</li>
+                    <li>42</li>
+                    <li>44</li>
+                </ul>
+                <div class="group">
+                    {{-- <h2><sup>€</sup>199<small>.99</small></h2> --}}
+                    <a href="#">Leggi tutto</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="imgbx">
+                <img src="https://static.nike.com/a/images/t_PDP_864_v1/f_auto,b_rgb:f5f5f5/7e90a06d-13fa-46d0-8cde-e3f3f0c61363/air-max-pre-day-be-true-shoes-gjZtQJ.png" alt="">
+                
+            </div>
+            <div class="details">
+                <h3>Nome dell'oliva</h3>
+                <h4>Dettaglio prodotto</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit
+                    . Maxime distinctio omnis necessitatibus
+                </p>
+                <h4>Formati disponibili</h4>
+                <ul>
+                    <li>36</li>
+                    <li>38</li>
+                    <li>40</li>
+                    <li>42</li>
+                    <li>44</li>
+                </ul>
+                <div class="group">
+                    {{-- <h2><sup>€</sup>199<small>.99</small></h2> --}}
+                    <a href="#">Leggi tutto</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</x-layout>
