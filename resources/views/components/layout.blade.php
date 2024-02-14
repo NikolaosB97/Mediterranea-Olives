d203300c296a64f9d5ecfd37d10b1853ee2ec15e
Bennylnz <bennylanza@gmail.com>
2024-02-14 10:26:30 +0100

    sistemata favicon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Mediterranea Olives</title>
+    <link rel="icon" href="{{ asset('img/dea.png') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
